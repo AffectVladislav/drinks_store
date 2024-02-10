a0b72c6f3443aef9196c672ae2d50b4170b18bdd
vladh <vlad.hagai@mai.ru>
2024-02-10 17:18:42 +0300

    TIK-13: прописал корзину сайта. Нужно дописать HTML чтобы все, корректно выводилось

--- store/cart/urls.py
+++ store/cart/urls.py
@@ -1,10 +1,10 @@
-#   from django.conf import settings
-# from django.conf.urls.static import static
-# from django.contrib import admin
-# from django.urls import path
-#
-# app_name = 'cart'
-#
-# urlpatterns = [
-#     path('', cart, name='cart'),
-# ]
+from django.urls import path
+from . import views
+
+app_name = 'cart'
+
+urlpatterns = [
+    path('', views.cart_detail, name='cart_detail'),
+    path('add/<int:product_id>/', views.cart_add, name='cart_add'),
+    path('remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
+]
--- store/shop/views.py
+++ store/shop/views.py
@@ -4,13 +4,9 @@ from .paginator import paginator
 
 def store(request):
     products = Product.objects.filter(available=True).order_by('?')
-    context = {'page_obj': paginator(request, products, 6)}
+    context = {'page_obj': paginator(request, products, 6),
+               'other_product': paginator(request, products, 3)}
     return render(request, 'shop/product/main_page.html', context)
-
-def other_products(request):
-    context = {'other_prod': paginator(request, products, 3)}
-    return render(request, 'shop/product/main_page.html', context)
-
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product,
@@ -18,3 +14,14 @@ def product_detail(request, id, slug):
                                 slug=slug,
                                 available=True)
     return render(request, 'shop/product/detail.html', {'product': product})
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, Slug=category_slug)
+        products = products.filter(category=category)
+    return render(request, 'shop/product/list.html', {'category': category,
+                                                      'categories': categories,
+                                                      'products': products})
