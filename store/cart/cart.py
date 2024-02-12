a3c940cb6e5363f921b9216091db993e3bc756da
vladh <vlad.hagai@mai.ru>
2024-02-12 17:28:36 +0300

    TIK-15: cart mtv

--- store/cart/cart.py
+++ store/cart/cart.py
@@ -10,6 +10,17 @@ class Cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
     def add(self, product, quantity=1, override_quantity=False):
         product_id = str(product.id)
         if product_id not in self.cart:
@@ -29,17 +40,6 @@ class Cart:
         if product_id in self.cart:
             del self.cart[product_id]   # удаление из словаря строки
             self.save()
-
-    def __iter__(self):
-        product_ids = self.cart.keys()
-        products = Product.objects.filter(id__in=product_ids)
-        cart = self.cart.copy()
-        for product in products:
-            cart[str(product.id)]['product'] = product
-        for item in cart.values():
-            item['price'] = Decimal(item['price'])
-            item['total_price'] = item['price'] * item['quantity']
-            yield item
 
     def __len__(self):
         return sum(item['quantity'] for item in self.cart.values())
